c8de565f093a71d0e41ef74aa22d2e4f76fae10f
GerlakhA <andrey215gerlakh@gmail.com>
2025-01-18 19:41:13 +0300

    added layout and header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <div class="w-[90%] h-screen m-auto bg-white rounded-2xl shadow-xl mt-20 mb-20">
+    <Header />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import clsx from 'clsx'
+
+const links = [
+  {
+    id: 1,
+    imgUrl: '/cart.svg',
+    item: '1205 ₽.',
+  },
+  {
+    id: 2,
+    imgUrl: '/heart.svg',
+    item: 'Закладки',
+  },
+  {
+    id: 3,
+    imgUrl: '/profile.svg',
+    item: 'Профиль',
+  },
+]
+
+const classFn = (id: number) =>
+  clsx(
+    'flex items-center gap-2 w-[125px] p-2 hover:bg-neutral-300/40 transition cursor-pointer rounded-lg',
+    id === 2 && 'text-slate-400 font-lg',
+    id === 3 && 'text-slate-400 font-lg',
+  )
+</script>
+
+<template>
+  <header class="w-full flex justify-between items-center border-b border-b-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div class="">
+        <h1 class="font-bold text-xl uppercase">Vue Sneakers</h1>
+        <p class="text-neutral-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <div class="flex items-center justify-around w-[650px]">
+      <div v-for="link in links" :key="link.id" :class="classFn(link.id)">
+        <img :src="link.imgUrl" :alt="link.item" class="w-5 h-5" />
+        <span class="font-bold">{{ link.item }}</span>
+      </div>
+    </div>
+  </header>
+</template>
